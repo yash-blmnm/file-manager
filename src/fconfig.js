3c820773de9683bc108b7e95f03a0e956de0bac6
Yasaswini <yasha.balu25@gmail.com>
2021-05-08 23:18:52 +0530

    hide firebase config

--- src/fconfig.js
+++ src/fconfig.js
@@ -6,13 +6,13 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 export const config = {
-  apiKey: "AIzaSyCfQ4E88IcYyIOk2ZbLi7SnTJL2EZbLtOk",
-  authDomain: "file-manager-7028c.firebaseapp.com",
-  databaseURL: "https://file-manager-7028c-default-rtdb.firebaseio.com",
-  projectId: "file-manager-7028c",
-  storageBucket: "file-manager-7028c.appspot.com",
-  messagingSenderId: "673189975286",
-  appId: "1:673189975286:web:b9f29d736db7c86db348d7",
-  measurementId: "G-8LS8F2L4B3"
+  apiKey: "API KEY",
+  authDomain: "DOMAIN",
+  databaseURL: "DB URL",
+  projectId: "ID",
+  storageBucket: "BUCKET",
+  messagingSenderId: "MSG SENDER ID",
+  appId: "APP ID",
+  measurementId: "M-ID"
 };
 
